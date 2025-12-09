0b3b243005d9a07d287fd59e1277f8b5c07ddd58
shraddhaNagarajuc <shraddhagowda73@gmail.com>
2025-12-09 22:49:17 +0530

    Configure for Vercel deployment (remove base path)

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/AUGMENT-ED/",
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
